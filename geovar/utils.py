fc0fc09b72a2f03f431269067ff502ac61b54459
Arjun Biddanda <aabiddanda@gmail.com>
2020-08-15 09:30:42 -0500

    added in final method to generate an allele frequency table from a VCF file

--- geovar/utils.py
+++ geovar/utils.py
@@ -1,14 +1,15 @@
 
 import allel
 import pandas as pd
-import numpy as np 
+import numpy as np
+from tqdm import tqdm
 
 def vcf_to_freq_table(vcf_file, pop_panel, minor_allele=True):
     """
         Converts a VCF File to a frequency table 
     """
     # NOTE: we only assume that we have two columns in hte 
-    pop_df = pd.read_table(pop_panel, usecols=['sample','pop'])
+    pop_df = pd.read_table(pop_panel, sep='\s', usecols=['sample','pop'])
     pop_dict = pop_df.set_index(['sample']).to_dict()['pop']     
     # NOTE: we are assuming that we have only biallelic markers
     vcf_data = allel.read_vcf(vcf_file, alt_number=1)
@@ -39,22 +40,25 @@ def vcf_to_freq_table(vcf_file, pop_panel, minor_allele=True):
         for i in flip_idx:
             global_ac[i] = tot_ac_cnt[i,0]
             global_af[i] = 1. - global_af[i]
-    print("HERE!")
+            # Swap the alleles here ... 
+            cur_ref = ref_alleles[i]
+            ref_alleles[i] = alt_alleles[i] 
+            alt_alleles[i] = cur_ref
     # Generate ALT AF per subpopulation 
     allel_cnt_subpops = gt.count_alleles_subpops(pop_idx_dict)
     flip_allele = ~flip_af
     flip_allele = flip_allele.astype(np.int8) 
-    f = lambda acnt, flip: acnt[:,flip]/acnt.sum(axis=1)
+    f = lambda acnt, flip: (acnt[np.arange(len(acnt)),flip]/acnt.sum(axis=1)).tolist()
     # Setting up the final data frame
     af_dict = {i: f(allel_cnt_subpops[i], flip_allele) for i in allel_cnt_subpops}
-    af_dict2 = {i: af_dict[i].tolist() for i in af_dict}
-    # af_df = pd.DataFrame(af_dict)
-    # # Inserting all of the columns that are needed
-    # af_df = af_df.insert(0, 'CHR', chrom)
-    # af_df = af_df.insert(1, 'SNP', pos)
-    # af_df = af_df.insert(2, 'A1', ref_alleles)
-    # af_df = af_df.insert(3, 'A2', alt_alleles)
-    # af_df = af_df.insert(4, 'MAC', global_ac)
-    # af_df = af_df.insert(5, 'MAF', global_af)
-    return(af_dict2)
+    af_df = pd.DataFrame(af_dict)
+    # Inserting all of the columns that are needed for the allele frequency 
+    af_df.insert(0, 'CHR', chrom)
+    af_df.insert(1, 'SNP', pos)
+    af_df.insert(2, 'A1', ref_alleles)
+    af_df.insert(3, 'A2', alt_alleles)
+    af_df.insert(4, 'MAC', global_ac)
+    af_df.insert(5, 'MAF', global_af)
+    return(af_df)
+
 
