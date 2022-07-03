8aa7ce6ce1990027a0ceb70d27f4d831790e3ce8
Arjun Biddanda <aabiddanda@gmail.com>
2022-07-03 16:56:37 -0400

    draft: small commit - still needs some fixing on docs

--- geovar/__init__.py
+++ geovar/__init__.py
@@ -4,10 +4,16 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-# from .utils import vcf_to_freq_table, sep_freq_mat_pops
-# from .binning import GeoVar
-# from .viz import GeoVarPlot
+from .utils import vcf_to_freq_table, sep_freq_mat_pops, read_pop_panel
+from .binning import GeoVar
+from .viz import GeoVarPlot
 
-# __all__ = ["GeoVar", "GeoVarPlot", "vcf_to_freq_table", "sep_freq_mat_pops"]
+__all__ = [
+    "GeoVar",
+    "GeoVarPlot",
+    "vcf_to_freq_table",
+    "sep_freq_mat_pops",
+    "read_pop_panel",
+]
 
 __version__ = "0.1.0"
