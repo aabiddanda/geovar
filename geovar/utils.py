31867eb98fc315ad6cbdfb10c17d83af24e53c38
Arjun Biddanda <aabiddanda@gmail.com>
2022-09-05 18:15:15 -0400

    feat: finally got cyvcf2 features working

--- geovar/utils.py
+++ geovar/utils.py
@@ -37,17 +37,20 @@ def verify_sample_indices(pop_df, samples):
         samples (:obj:`list`): list of sample IDs (strings) that are ready to be indexed.
 
     """
+    assert "sample" in pop_df.columns
+    assert "pop" in pop_df.columns
     if type(samples) == list:
         samples = np.asarray(samples, dtype=str)
     pop_dict = pop_df.set_index(["sample"]).to_dict()["pop"]
     unique_pops = np.unique([pop_dict[i] for i in pop_dict])
-    pop_vector = np.repeat("", samples.size)
+    pop_vector = []
     for i, s in enumerate(samples):
         try:
-            pop_vector[i] = pop_dict[s]
+            pop_vector.append(pop_dict[s])
         except KeyError:
+            pop_vector.append("")
             warnings.warn(f"Sample {s} does not have a population label!", UserWarning)
-    pop_vector = np.array(pop_vector)
+    pop_vector = np.asarray(pop_vector, dtype=str)
     pop_idx_dict = {}
     for p in unique_pops:
         pop_idx_dict[p] = np.where(pop_vector == p)[0]
@@ -92,36 +95,43 @@ def vcf_to_freq_table(vcf_file, pop_df, outfile=None, minor_allele=True, **kwarg
     alt_alleles = []
     global_af = []
     global_ac = []
+    global_n = []
     alt_freq = []
     allele_cnt_subpops = []
     for variant in tqdm(vcf):
         chrom.append(variant.CHROM)
         pos.append(variant.POS)
         ref_alleles.append(variant.REF)
-        alt_alleles.append(variant.ALT)
-        global_ac.append(variant.AC)
+        # NOTE: We only take the first alternative allele here ...
+        alt_alleles.append(variant.ALT[0])
         alt_freq.append(variant.aaf)
-        cur_gt = variant.gt_types.copy()
-        pop_ac_cnt = [
-            cur_gt[pop_idx_dict[i].sum() / pop_idx_dict[i].size] for i in unique_pops
-        ]
+        global_ac.append(variant.num_het + 2 * variant.num_hom_alt)
+        global_n.append(variant.num_called)
+        cur_gt = variant.gt_types
+        pop_ac_cnt = []
+        for i in unique_pops:
+            ac = np.sum(cur_gt[pop_idx_dict[i]] == vcf.HET) + 2 * np.sum(
+                cur_gt[pop_idx_dict[i]] == vcf.HOM_ALT
+            )
+            n = np.sum(cur_gt[pop_idx_dict[i]] != vcf.UNKNOWN)
+            pop_ac_cnt.append(ac / (2 * n))
         allele_cnt_subpops.append(pop_ac_cnt)
-
+    alt_freq = np.array(alt_freq, dtype=np.float32)
     allele_cnt_subpops = np.array(allele_cnt_subpops)
     global_af = alt_freq
-    flip_af = np.repeat(False, alt_freq.size)
+    flip_af = np.repeat(False, len(alt_freq))
     if minor_allele:
         flip_af = alt_freq > 0.5
     flip_idx = np.where(flip_af)[0]
     if flip_idx.size > 0:
         for i in flip_idx:
-            global_ac[i] = 2 * vcf.samples.size - global_ac[i]
+            global_ac[i] = global_n[i] - global_ac[i]
             global_af[i] = 1.0 - global_af[i]
             # Swap the alleles here ...
             cur_ref = ref_alleles[i]
             ref_alleles[i] = alt_alleles[i]
             alt_alleles[i] = cur_ref
-            allele_cnt_subpops[i, :] = 1.0 - allele_cnt_subpops
+            allele_cnt_subpops[i, :] = 1.0 - allele_cnt_subpops[i, :]
     # Setting up the final data frame
     af_dict = {x: allele_cnt_subpops[:, i] for (i, x) in enumerate(unique_pops)}
     af_df = pd.DataFrame(af_dict)
