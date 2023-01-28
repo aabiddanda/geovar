0c2b2a37cba828ae01946388ecb77ba769452c84
Arjun Biddanda <aabiddanda@gmail.com>
2023-01-28 11:54:57 -0500

    test: added in a test for gzipped frequency files

--- geovar/binning.py
+++ geovar/binning.py
@@ -46,7 +46,7 @@ class GeoVar(object):
             (see example notebook for formatting).
 
         """
-        af_df = pd.read_table(freq_mat_file, sep=r"\s")
+        af_df = pd.read_table(freq_mat_file, sep=r"\s", engine='python')
         pops, freq_mat = sep_freq_mat_pops(af_df)
         self.pops = pops
         self.freq_mat = freq_mat
@@ -60,14 +60,16 @@ class GeoVar(object):
             bins (:obj:`list`): list of tuples specifying bins of allele frequency.
 
         """
-        assert np.all(np.array(bins) < 1.0)
-        b = 0.0
-        new_bins = []
-        for x in bins:
-            new_bins.append((b, x))
-            b = x
-        new_bins.append((b, 1.0))
-        self.bins = new_bins
+        assert np.all(np.array(bins) <= 1.0)
+        assert np.all(np.array(bins) >= 0.0)
+        min_val = 1.0
+        max_val = 0.0
+        for (start,end) in bins:
+            min_val = min(min_val, start)
+            max_val = max(max_val, end)
+        assert min_val >= 0
+        assert max_val <= 1
+        self.bins = bins
 
     def geovar_binning(self):
         """Compute the GeoVar codes for each variant across each population."""
@@ -95,7 +97,7 @@ class GeoVar(object):
         Args:
             freq_mat_file (:obj:`string`): filepath to a frequency table file (see example notebook for formatting).
         """
-        
+
         assert self.bins is not None
         freq_mat_fp = Path(freq_mat_file)
         assert freq_mat_fp.is_file()
