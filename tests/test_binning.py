14a82cf13645291bda8d47a1d7aa5a14e5c6c675
Arjun Biddanda <aabiddanda@users.noreply.github.com>
2023-01-28 12:13:49 -0500

    Gzip streaming + additional tests (#9)
    * Dev (#6)
    
    * setup initial tests
    
    * test: updated testing actions and test structure
    
    * feat: added in some tests for utils
    
    * feat: added in function for reading in population panel files separately
    
    * draft: updated some tests
    
    * test: population panel testing
    
    * feat: new function to add in population handling
    
    * test: added in test for verification of samples
    
    * feat: vcf2frequency table now uses cyvcf2
    
    * draft: small commit - still needs some fixing on docs
    
    * test: some refactoring and some testing
    
    * doc: some additional docstrings
    
    * feat: updated to include data in installation
    
    * feat: finally got cyvcf2 features working
    
    * feat: not supporting windows due to cyvcf2
    
    * feat: added update to make custom lists a little bit easier
    
    * doc: cleaned up a lot of the original documentation
    
    * fix: initial attempt to address issue #8
    
    * test: updated tests for utilities to be more comprehensive
    
    * test: added in a test for gzipped frequency files
    
    * ci: updated some of the github CI parameters

--- tests/test_binning.py
+++ tests/test_binning.py
@@ -15,6 +15,11 @@ def bins_greater_than_one():
     """Bins that are greater than one."""
     return [(0, 0.5), (0.5, 1.1)]
 
+@pytest.fixture
+def bins_alt1():
+    """An alternative binning structure."""
+    return [(0, 0), (0, 0.01), (0.01, 0.05), (0.05, 1.0)]
+
 
 def test_bin_boundaries(bins_less_than_zero, bins_greater_than_one):
     """Testing the bin boundaries."""
@@ -28,3 +33,58 @@ def test_empty_bins():
     """Test that the bins are not empty."""
     with pytest.raises(AssertionError):
         GeoVar(bins=[])
+
+
+def test_generate_bins(bins_less_than_zero, bins_greater_than_one):
+    """Test the new generation of bins."""
+    geov_obj = GeoVar()
+    with pytest.raises(AssertionError):
+        geov_obj.generate_bins(bins=bins_less_than_zero)
+    with pytest.raises(AssertionError):
+        geov_obj.generate_bins(bins=bins_greater_than_one)
+    geov_obj.generate_bins(bins=[(0, 0), (0, 0.01), (0.01, 1.0)])
+
+
+@pytest.fixture
+def valid_freq_mat():
+    """A Valid frequency matrix for input to geovar."""
+    return 'geovar/data/new_1kg_nygc.chr22.biallelic_snps.filt.n5000.freq.txt'
+
+@pytest.fixture
+def valid_freq_mat_gz():
+    """A Valid frequency matrix for input to geovar as a gzipped file."""
+    return 'geovar/data/new_1kg_nygc.chr22.biallelic_snps.filt.n5000.freq.txt.gz'
+
+def test_add_freq_mat(valid_freq_mat):
+    """Test for addition of a valid frequency matrix file for GeoVar."""
+    geov_obj = GeoVar()
+    geov_obj.add_freq_mat(valid_freq_mat)
+
+
+def test_geovar_binning(valid_freq_mat, bins_alt1):
+    """Test GeoVar binning under multiple binning schemes."""
+    geov_obj = GeoVar()
+    geov_obj.add_freq_mat(valid_freq_mat)
+    geov_obj.geovar_binning()
+    geov_obj.generate_bins(bins_alt1)
+    geov_obj.geovar_binning()
+
+def test_count_geovar_codes(valid_freq_mat, bins_alt1):
+    geov_obj = GeoVar()
+    geov_obj.add_freq_mat(valid_freq_mat)
+    geov_obj.geovar_binning()
+    u, n_geovar, ncat = geov_obj.count_geovar_codes()
+    # Two non-zero categories ...
+    assert ncat == 2
+    assert all(n_geovar > 0)
+
+
+
+def test_geovar_codes_streaming(valid_freq_mat, valid_freq_mat_gz):
+    geov_obj = GeoVar()
+    geov_obj.geovar_codes_streaming(valid_freq_mat)
+    assert geov_obj.n_variants == 5000
+    assert geov_obj.pops.size == geov_obj.n_populations
+    geov_obj.geovar_codes_streaming(valid_freq_mat_gz)
+    assert geov_obj.n_variants == 5000
+    assert geov_obj.pops.size == geov_obj.n_populations
